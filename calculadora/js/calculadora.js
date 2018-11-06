1e827cb118cf4d62f1af6a9fc0f7934897f2062c
Luis Eduardo Ramos <luiseduardoramosg@gmail.com>
2018-11-06 09:59:05 -0700

    calculadora

---
+++ calculadora/js/calculadora.js
@@ -0,0 +1,100 @@
+window.onload=function(){
+	var operador = ""
+	var colorAmarillo = function(){
+	this.style.background = "yellow"
+}
+var colorBlanco = function(){
+	this.style.background = "white"
+}		
+
+var numeros= function(){
+	var valor=this.value;
+	if (operador==""){
+		var valorInput=document.getElementById("operando1").value;
+		if (valorInput=="0"){
+			document.getElementById("operando1").value="";
+		}
+		document.getElementById("operando1").value+=valor;
+	}
+	else {//operando2
+		
+		var valorInput=document.getElementById("operando2").value;
+		if (valorInput=="0"){
+			document.getElementById("operando2").value="";
+		}
+		document.getElementById("operando2").value+=valor;	
+	}
+}		
+var operadores = function(){
+	operador = this.value
+	console.log(operador)
+}
+var resultadoIgual = function(){
+	oper1=document.getElementById('operando1').value
+	oper2=document.getElementById('operando2').value
+	document.getElementById('resultado').value=eval(oper1+operador+oper2)
+}
+var reinicio = function(){
+	document.getElementById('operando1').value="0"
+	document.getElementById('operando2').value="0"
+	document.getElementById('resultado').value="0"
+	operador=""
+}
+
+var operando1=document.getElementById("operando1")
+	operando1.addEventListener("focus", colorAmarillo)
+	operando1.addEventListener("focusout", colorBlanco)
+
+var operando2=document.getElementById("operando2")
+	operando2.addEventListener("focus", colorAmarillo)
+	operando2.addEventListener("focusout", colorBlanco)
+
+var cero=document.getElementById("cero")
+	cero.addEventListener("click",numeros)
+
+var uno=document.getElementById("uno")
+	uno.addEventListener("click",numeros)
+
+var dos=document.getElementById("dos")
+	dos.addEventListener("click",numeros)
+
+var tres=document.getElementById("tres")
+	tres.addEventListener("click",numeros)
+
+var cuatro=document.getElementById("cuatro")
+	cuatro.addEventListener("click",numeros)
+
+var cinco=document.getElementById("cinco")
+	cinco.addEventListener("click",numeros)
+
+var seis=document.getElementById("seis")
+	seis.addEventListener("click",numeros)
+
+var siete=document.getElementById("siete")
+	siete.addEventListener("click",numeros)
+
+var ocho=document.getElementById("ocho")
+	ocho.addEventListener("click",numeros)
+
+var nueve=document.getElementById("nueve")
+	nueve.addEventListener("click",numeros)
+
+var suma=document.getElementById("suma")
+	suma.addEventListener("click",numeros)
+ 
+var resta=document.getElementById("resta")
+	resta.addEventListener("click",numeros)
+
+var multiplicacion=document.getElementById("multiplicacion")
+	multiplicacion.addEventListener("click",numeros)
+
+var division=document.getElementById("division")
+	division.addEventListener("click",numeros)
+
+var igual=document.getElementById("igual")
+	igual.addEventListener("click",numeros)
+
+var reiniciar=document.getElementById("reiniciar")
+	reiniciar.addEventListener("click",reinicio)
+
+}
